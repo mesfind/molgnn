b78cda8975d45193ec52db7e1ba1030e8265efc7
Mesfin Diro (PhD) <mesfin.diro@aau.edu.et>
2025-01-11 10:13:30 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,26 +1,15 @@
 
 from types import ModuleType
 from importlib import import_module
-
-
 import torch_geometric.loader
 import torch_geometric.transforms
 import torch_geometric.utils
 import torch_geometric.profile
-<<<<<<< HEAD
-=======
-
->>>>>>> 29045e8145848d15355692a1de3067107f206d91
 import molgnn.data
 from molgnn.pygdata import RedDB
 from molgnn.molgnn import MolGCNConv, MolGATConv
 from molgnn.molfeatures import GenMolGraph, GenMolecules, GenMolFeatures
-<<<<<<< HEAD
-=======
 from molgnn.utils.utils import NoamLR
->>>>>>> 29045e8145848d15355692a1de3067107f206d91
-
-
 
 __version__ = '1.0'
 
